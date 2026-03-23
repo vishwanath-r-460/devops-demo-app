5e998be03d9a54612dcb1a22329eb34c0ed4207a
vishwanath-r-460 <vishwauchiha@outlook.com>
2026-03-23 15:11:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from DevOps Demo App!\n');
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
+});
