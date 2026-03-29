9f069642af89ef365f89314a8598dcc6b2238be5
vishwanath-r-460 <vishwauchiha@outlook.com>
2026-03-30 00:04:25 +0530

    testing the auto deployment in changed order of pipline

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const HOST = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from DevOps Demo App v3!\n');
+  res.end('Hello from DevOps Demo App v4!\n');
 });
 
 server.listen(PORT, HOST, () => {
