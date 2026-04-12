142d5db18d7870bb720c14e35aef9a13ea7cf2c8
vishwanath-r-460 <vishwauchiha@outlook.com>
2026-04-12 15:35:58 +0530

    testing v6 for rollback

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const HOST = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from DevOps Demo App v5!\n');
+  res.end('Hello from DevOps Demo App v6!\n');
 });
 
 server.listen(PORT, HOST, () => {
