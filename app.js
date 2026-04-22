2f4432e28bc572da146fb59c69e38f73e058b301
vishwanath-r-460 <vishwauchiha@outlook.com>
2026-04-22 14:55:32 +0530

    version changed to v8

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const HOST = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from DevOps Demo App v7!\n');
+  res.end('Hello from DevOps Demo App v8!\n');
 });
 
 server.listen(PORT, HOST, () => {
