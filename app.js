e72f8b2e3aba3104726f294e816f2dcb91929812
vishwanath-r-460 <vishwauchiha@outlook.com>
2026-04-27 14:33:22 +0530

    chenking rollback 28v

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const http = require('http');
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+// ❌ Crash immediately (before server starts)
+console.log("App failed to start!");
+process.exit(1);
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from DevOps Demo App v10\n');
+  res.end('This should never run\n');
 });
 
 server.listen(PORT, HOST, () => {
