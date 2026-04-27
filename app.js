15eee466937048c267164334a1238e3494249bbc
vishwanath-r-460 <vishwauchiha@outlook.com>
2026-04-27 13:34:41 +0530

    checking the script before rollback

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const HOST = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hello devops v10\n');
+  res.end('hello from devops demo app v10\n');
 });
 
 server.listen(PORT, HOST, () => {
