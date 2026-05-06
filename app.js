148201de374f6a8b39b7e80fdc24b128424367ce
vishwanath-r-460 <vishwauchiha@outlook.com>
2026-05-06 14:51:40 +0530

    fixed compose crash

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const http = require('http');
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-// ❌ Crash immediately (before server starts)
-console.log("App failed to start!");
-process.exit(1);
-
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('This should never run\n');
+  res.end('Hello from DevOps Demo App v11\n');
 });
 
 server.listen(PORT, HOST, () => {
